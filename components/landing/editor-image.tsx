46e0aa30bc2d50b8a8b36139d5a78a14d4991b5f
Syed Sharukh Hussainy <sharukhsyed.hussainy@gmail.com>
2023-10-09 02:05:29 +0530

    Fix: fix bug with theme changing to system as a string.

--- components/landing/editor-image.tsx
+++ components/landing/editor-image.tsx
@@ -2,30 +2,24 @@
 
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 const EditorImage = () => {
-  const { theme, systemTheme } = useTheme();
-  const selectedTheme =
-    theme === 'dark' || theme === 'light' ? theme : systemTheme;
-  const [themes, setThemes] = useState(selectedTheme);
-
-  useEffect(() => {
-    setThemes(selectedTheme);
-  }, [selectedTheme]);
+  const { resolvedTheme } = useTheme();
 
   return (
     <section className="flex justify-center my-16 px-5 md:my-32 md:px-0">
       <section className="flex items-center justify-center bg-primary w-full max-w-7xl h-[250px] md:h-[700px] rounded-xl md:rounded-lg">
-        <Image
-          src={`https://ik.imagekit.io/x7lxfswax/${themes}-editor-bg.webp`}
-          alt="screenshot-editor"
-          width={900}
-          height={900}
-          loading="eager"
-          priority={true}
-          className="object-contain rounded-md md:rounded-xl shadow-xl w-[300px] md:w-[1100px] h-auto"
-        />
+        {resolvedTheme ? (
+          <Image
+            src={`https://ik.imagekit.io/x7lxfswax/${resolvedTheme}-editor-bg.webp`}
+            alt="screenshot-editor"
+            width={900}
+            height={900}
+            loading="eager"
+            priority={true}
+            className="object-contain rounded-md md:rounded-xl shadow-xl w-[300px] md:w-[1100px] h-auto"
+          />
+        ) : null}
       </section>
     </section>
   );
