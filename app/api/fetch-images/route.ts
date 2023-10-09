619612aa6ec4a92e6aeaadbb45279c864b0d34ac
Syed Sharukh Hussainy <sharukhsyed.hussainy@gmail.com>
2023-10-09 16:12:45 +0530

    Fetch images from s3 bucket

---
+++ app/api/fetch-images/route.ts
@@ -0,0 +1,38 @@
+import {
+  ListObjectsCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { NextResponse } from 'next/server';
+
+const s3Client = new S3Client({
+  region: process.env.REGION as string,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+  },
+});
+
+export async function GET() {
+  try {
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      prefix: 'themes/',
+    };
+
+    const command = new ListObjectsCommand(params);
+    const response = await s3Client.send(command);
+
+    const images = response.Contents?.filter((object) => {
+      if (object.Key !== 'themes/' && object.Key?.includes('themes/')) {
+        return {
+          Key: object.Key,
+        };
+      }
+    });
+    return NextResponse.json({ success: true, images });
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    NextResponse.json({ message: 'Error fetching image' });
+  }
+}
