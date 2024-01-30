609fbc5751d2253100b8600d4a090d380b0ca433
Syed Sharukh Hussainy <sharukhsyed.hussainy@gmail.com>
2024-01-30 12:15:08 +0530

    some changes

--- app/api/fetch-images/route.ts
+++ app/api/fetch-images/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from "next/server";
 const s3Client = new S3Client({
   region: process.env.REGION as string,
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+    accessKeyId: process.env.ACCESS_KEY_ID!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
 });
 
